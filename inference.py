10b90627d86ce9260c80faa35c570bf22c797597
stiyqr <stiyqr@gmail.com>
2024-11-14 00:42:40 +0800

    change percentage text location

--- inference.py
+++ inference.py
@@ -472,11 +472,11 @@ def face_rect_multiple(images):
 
             # Draw a label with a name below the face
             font = cv2.FONT_HERSHEY_DUPLEX
-            name_text = f"{name} - {percentage * 100:.2f}%"
             # cv2.rectangle(image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             # cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
             cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 200), 2)
-            cv2.putText(image, name_text, (left, top - 10), font, 1, (200, 0, 0), 2)
+            cv2.putText(image, name, (left, top - 10), font, 1, (200, 0, 0), 2)
+            cv2.putText(image, f"{percentage * 100:.2f}%", (left + 6, bottom - 6), font, 1, (200, 0, 0), 1)
 
             if name == args.speaker:
                 box_list = [left, top, right, bottom]
